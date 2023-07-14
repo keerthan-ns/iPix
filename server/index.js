d7a0bd18a00d0d994ce85472e464de2c2395a5a8
Keerthan N S <keerthanns091@gmail.com>
2023-07-14 11:50:41 +0530

    [add]user model

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from "express"
 import bodyParser from "body-parser"
-import mongoose from "mongoose"
+import mongoose, { mongo } from "mongoose"
 import cors from "cors"
 import dotenv from "dotenv"
 import multer from "multer"
@@ -8,6 +8,8 @@ import helmet from "helmet"
 import morgan from "morgan"
 import path from "path"
 import { fileURLToPath } from "url"
+import { error } from "console"
+import {register} from "./controllers/auth.js"
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url)
@@ -36,3 +38,19 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({storage})
+
+// ROUTES WITH FILES
+app.post("/auth/register",upload.single("picture"),register)//register is termed as controller
+
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 6001
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser : true,
+    useUnifiedTopology: true,
+}).then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server: ${PORT}`)
+    })
+}).catch((error)=>{
+    console.error(`Could not connect : ${error}`)
+})
